34e184d909902807d90941cb40a57dc2eecc63ea
luxeave <luxeaves@gmail.com>
2024-02-22 07:16:02 +0700

    Initial commit.

---
+++ src/rbank_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { rbank_backend } from "../../declarations/rbank_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await rbank_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
